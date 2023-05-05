a10721673d841759fa68ecaa70d5bf43c2fe44d5
Yosra Nabil <yoyitanabil@gmail.com>
2023-05-06 02:03:59 +0300

    Added App Name in SplashScreen

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:practice/Screens/onboarding/onboarding_screen.dart';
@@ -14,14 +15,30 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: Lottie.asset(
-        'Assets/lotties/99280-beezhive-splash-loading-animation.json',
-        height: 350,
-        width: 350,
+      splash: Column(
+        children: [
+          Lottie.asset(
+            'Assets/lotties/99280-beezhive-splash-loading-animation.json',
+            height: 203,
+            width: 400,
+          ),
+          DefaultTextStyle(
+          style: const TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.amber,
+          ),
+          child: AnimatedTextKit(
+              animatedTexts: [
+                TyperAnimatedText('Forecastly'),
+              ],
+          ),
+        ),
+        ],
       ),
       nextScreen: const onboardingScreen(),
       splashIconSize: 250,
-      duration: 5000,
+      duration: 5000000000000000000,
       splashTransition: SplashTransition.fadeTransition,
       animationDuration: const Duration(seconds: 2),
       backgroundColor: Colors.deepPurpleAccent,
